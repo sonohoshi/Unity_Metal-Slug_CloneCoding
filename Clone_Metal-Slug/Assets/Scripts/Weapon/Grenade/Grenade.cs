7191a22b45de6e903f382f748d3d90948fadf3b5
sonohoshi <yd111544@gmail.com>
2020-10-01 00:05:53 +0900

    Implement Equals method in Grenade

--- Clone_Metal-Slug/Assets/Scripts/Weapon/Grenade/Grenade.cs
+++ Clone_Metal-Slug/Assets/Scripts/Weapon/Grenade/Grenade.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Grenades
+{
+    Bomb = 0,
+    Fire = 1,
+    Stone = 2
+}
+
 public class Grenade : MonoBehaviour
 {
+    public Grenades GrenadeType;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public override bool Equals(object other)
     {
-        
+        var otherGrenade = other as Grenade;
+        if (otherGrenade != null)
+        {
+            return otherGrenade.GrenadeType == GrenadeType;
+        }
+        return false;
     }
 }
