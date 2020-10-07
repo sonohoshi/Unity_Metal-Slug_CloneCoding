f878eb02e5e587ae1690267e9a78b6764a534cad
sonohoshi <yd111544@gmail.com>
2020-10-07 20:03:00 +0900

    Make InputManager

---
+++ Clone_Metal-Slug/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Object.Player;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private Player player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetInput();
+    }
+
+    private KeyCode? GetInput()
+    {
+        var horizontalMoving = Input.GetAxis("Horizontal");
+        if (horizontalMoving != 0)
+        {
+            var input = horizontalMoving > 0 ? KeyCode.RightArrow : KeyCode.LeftArrow;
+            player.Move(horizontalMoving);
+        }
+        return null;
+    }
+}
